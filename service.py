ae7b87f46341c04c5806558dcb4f6c24cf4b2bb9
Malik Eljaouadi <malik.eljaouadi@wefox.com>
2023-04-29 19:27:23 +0200

    serving a production ready bento

--- service.py
+++ service.py
@@ -3,6 +3,10 @@ import numpy as np
 from bentoml.io import NumpyNdarray
 
 # Get the saved model (in the model_packaging.py) from the BentoML local model store
+
+# to_runner creates a Runner instance from the model. The Runner abstraction gives BentoServer more flexibility in terms
+# of how to schedule the inference computation, how to dynamically batch inference calls and better take advantage of
+# all hardware resource available.
 iris_clf_runner =bentoml.sklearn.get("irid_clf:dx4rabxfvwocnkrb").to_runner()
 
 # Creating the service (a Service is the core componenet of BentoML that will allow us to expose the model to an API)
